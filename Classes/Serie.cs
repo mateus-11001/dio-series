9f0b398c339a1fdbd52f5a1177f75847c00a8a96
Mateus <mateusvlv@gmail.com>
2021-04-11 07:44:54 -0300

    commit inicial

---
+++ Classes/Serie.cs
@@ -0,0 +1,55 @@
+namespace series
+{
+    public class Serie : EntidadeBase
+    {
+        // Atributos
+        private Genero Genero { get; set; }
+        private string Titulo { get; set; }
+        private string Descricao { get; set; }
+        private int Ano {get; set; }
+        private bool Excluido {get; set; } 
+
+     // Métodos
+     public Serie(int id, Genero genero, string titulo, string descricao, int ano)
+    {
+        this.Id = id;
+        this.Genero = genero;
+        this.Titulo = titulo;
+        this.Descricao = descricao;
+        this.Ano = ano;
+        this.Excluido = false;
+    }
+
+        public override string ToString()
+        {
+            string retorno = "";            
+            retorno += "Gênero: " + this.Genero + "\n";//se nao der \n
+            retorno += "Titulo: " + this.Titulo + "\n";
+            retorno += "Descrição: " + this.Descricao + "\n";
+            retorno += "Ano de Início: " + this.Ano + "\n";
+            retorno += "Excluído: " + this.Excluido + "\n";
+            return retorno;
+        }
+
+        public string retornaTitulo()
+        {
+            return this.Titulo;
+        }
+
+        public int retornaId()
+        {
+            return this.Id;
+        }
+
+        public bool retornaExcluido()
+        {
+            return this.Excluido;
+        }
+
+        public void Excluir()
+        {
+            this.Excluido = true;
+        }
+    
+    }
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+**Aplicação básica de cadastro de séries**
+
+Uma aplicação básica de cadastros de séries de entretenimento.
